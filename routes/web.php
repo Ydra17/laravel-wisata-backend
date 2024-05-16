68d2c12d4cd0a02cd828180cdc8e730defbc64e6
Ydra17 <ardyantobaso@gmail.com>
2024-05-16 14:08:14 +0800

    install fortify

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('/home', function () {
     return view('pages.dashboard');
 });
